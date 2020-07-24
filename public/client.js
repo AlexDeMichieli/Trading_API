542457e9b4db0bc7629415fb3e746e190aa8c71f
Alexander De Michieli <ademichieli@squarespace.com>
2020-07-24 09:34:00 -0700

    saving

--- public/client.js
+++ public/client.js
@@ -1,32 +1,15 @@
-//having this set in the client should allow to select the symbol and subscribe when clicked
-//should close the connection and open a new one when clicked
-//should emit to the server and the server should emit back to the client ?
-socket.on('userconnected', (event)=>{
-
-  const ws = new WebSocket(`wss://ws.finnhub.io?token=`);
-
-// // Connection opened -> Subscribe
-ws.onopen = (event) => {
-  console.log('ws open')
-  ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'TSLA'}))
-  // ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
-  // ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
-};
-
-// // Listen for messages
-ws.onmessage = (event) => {
-    console.log(event.data)
-
-    //this stopped the straming!!
-    ws.send(JSON.stringify({'type':'unsubscribe','symbol': "TSLA"}))
-
-};
-
-ws.onclose = () => {
-  ws.send(JSON.stringify({'type':'unsubscribe','symbol': "TSLA"}))
-};
-// // Unsubscribe
-//  let unsubscribe = function(symbol) {
-//   ws.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+// const WebSocket = require('ws')
+// const url = 'ws://localhost:5000'
+// const connection = new WebSocket(url)
+ 
+// connection.onopen = () => {
+//   connection.send('Message From Client') 
 // }
-})
+ 
+// connection.onerror = (error) => {
+//   console.log(`WebSocket error: ${error}`)
+// }
+ 
+// connection.onmessage = (e) => {
+//   console.log(e.data)
+// }
--- server.js
+++ server.js
@@ -1,43 +1,21 @@
-const express = require('express')
-const app = express()
-const socketio = require('socket.io')
+const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
 
-const PORT = process.env.PORT || 5000 
-const expressServer = app.listen(PORT, '0.0.0.0', () => {
-    console.log('Server is running on port: ' + PORT)
-});
+const port = 5000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
 
-const io = socketio(expressServer)
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
 
-app.use(express.static(__dirname + '/public'));
-let array = []
-
-io.on('connection', (socket, req) => {
-
-  socket.emit('userconnected', socket.id);
-  
-//   const ws = new WebSocket(`wss://ws.finnhub.io?token=bsc3vsfrh5rau11sbktg`);
-
-// // Connection opened -> Subscribe
-// ws.onopen = (event) => {
-//   ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'TSLA'}))
-//   // ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
-//   // ws.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
-// };
-
-// // Listen for messages
-// ws.onmessage = (event) => {
-//     array.push(event.data)
-//     console.log(event.data)
-//     socket.emit('message',array);
-// };
-
-
-// // Unsubscribe
-//  let unsubscribe = function(symbol) {
-//   ws.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
-// }
-
-});
-
+server.listen(port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
