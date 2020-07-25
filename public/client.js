04109f581f12b25a2efeffb6c1afd99ffc24c170
Alexander De Michieli <ademichieli@squarespace.com>
2020-07-24 23:31:03 -0700

    saving

--- public/client.js
+++ public/client.js
@@ -1,13 +1,42 @@
-const router = require("express").Router();
-const WebSocket = require("ws");
 
-const broadcast = (clients, message) => {
+let subscribe = document.getElementById('subscribe')
+let unsubscribe = document.getElementById('unsubscribe')
+let active = false
 
-    clients.forEach((client) => {
+socket.on('userconnected', (event) =>{
 
-        if (client.readyState === WebSocket.OPEN) {
+    const socket = new WebSocket('wss://ws.finnhub.io?token=');
 
-            client.send(message);
-        }
+        socket.addEventListener('open', function (event) {
+
+            // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+        // subscribe.addEventListener("click", function(){
+        // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+        // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+        // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
+    // });
+});
+   
+
+    
+
+    // Connection opened -> Subscribe
+    
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
     });
-};
+    
+    // Unsubscribe
+    unsubscribe.addEventListener("click", function(symbol){
+        console.log('unsubscribed ')
+        active = false
+        socket.send(JSON.stringify({'type':'unsubscribe','symbol': "BINANCE:BTCUSDT"}))
+    })
+    subscribe.addEventListener("click", function(symbol){
+        socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+    })
+    
+    
+
+})
--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const { createServer } = require("http");
-const express = require("express");
-const WebSocket = require("ws");
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+const WebSocket = require('ws');
 
-const app = express();
-app.use(express.json({ extended: false }));
-app.use(express.static(__dirname + '/public'));
-
-const port = process.env.PORT || 5000;
-const server = createServer(app);
-server.listen(port, () => console.info(`Server running on port: ${port}`));
-
-const webSocketServer = new WebSocket.Server({ server });
-webSocketServer.on("connection", (ws) => {
-
-    console.info("Total connected clients:", webSocketServer.clients.size);
-
-    app.locals.clients = webSocketServer.clients;
-    ws.on('message', function incoming(data) {
-      webSocketServer.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      })
-    })
+const PORT = process.env.PORT || 5000 
+const expressServer = app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server is running on port: ' + PORT)
 });
 
+const io = socketio(expressServer)
+
+app.use(express.static(__dirname + '/public'));
+
+
+io.on('connection', (socket, req) => {
+
+  socket.emit('userconnected', socket.id);
+
+})
